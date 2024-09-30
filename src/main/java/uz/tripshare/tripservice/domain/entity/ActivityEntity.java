9807f3cf7c873e681b41d0f66968cfbc3af08f82
Javohir004 <javohirsharifjonov795@gmail.com>
2024-09-30 14:47:10 +0500

    yeah ahhaha

---
+++ src/main/java/uz/tripshare/tripservice/domain/entity/ActivityEntity.java
@@ -0,0 +1,24 @@
+package uz.tripshare.tripservice.domain.entity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import uz.tripshare.tripservice.domain.enumerators.Category;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "activity")
+public class ActivityEntity extends BaseEntity {
+    private String name;
+    private String description;
+    private String location;
+    private Double price;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+}
